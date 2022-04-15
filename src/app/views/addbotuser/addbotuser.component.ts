94a14bdf62b982ad7a199170cfbb6a71de20a52d
divangi-tagline <divangi.tagline@gmail.com>
2022-04-15 12:52:44 +0530

    Add toster for add,update,delete and data updated

--- src/app/views/addbotuser/addbotuser.component.ts
+++ src/app/views/addbotuser/addbotuser.component.ts
@@ -3,19 +3,21 @@ import { FormControl, FormGroup, Validators } from "@angular/forms";
 import { Router } from "@angular/router";
 import { User } from "../../models/user.model";
 import { UserService } from "../../shared/service/user.service";
-
+import { ToastrService } from 'ngx-toastr';
 @Component({
   selector: "app-addbotuser",
   templateUrl: "./addbotuser.component.html",
-  styleUrls: ["./addbotuser.component.css"],
+  styleUrls: ["./addbotuser.component.css"]
 })
 export class AddbotuserComponent implements OnInit {
   public Botuserform!: FormGroup;
   public users: any = [];
+  public submit = 'Submit';
+  public currentUserId:string = '';
   public itsUpdate: boolean = true;
   public buttonName = "Add"
   public h4="Add Bot User"
-  constructor(private userservice: UserService, private route: Router) { }
+  constructor(private userservice: UserService, private route: Router, private toastr: ToastrService) { }
 
   ngOnInit(): void {
     this.Botuserform = new FormGroup({
@@ -38,16 +40,18 @@ export class AddbotuserComponent implements OnInit {
 
   onSubmit(operation: string) {
     console.log('operation :>> ', operation);
-      if (this.buttonName === 'Add') {
+      if (this.currentUserId === '') {
         if (this.Botuserform.valid) {
           this.userservice.addUser(this.Botuserform.value).then((res: any) => {
             if (res) {
+              this.toastr.success('User Data Added!');
               this.Botuserform.reset()
             }
           });
         }
-      } else {
-        this.updateUser(this.Botuserform.value)
+      } 
+      else {
+        this.updateUser(this.Botuserform.value);
       }
 
   }
@@ -63,18 +67,27 @@ export class AddbotuserComponent implements OnInit {
   public deleteUser(id): void {
     if (confirm('are you sure you want to delete')) {
       this.userservice.remove(id);
+      this.toastr.error('User Data Deleted!');
     }
     this.getUserData();
   }
 
-  onEdit(item) {
+  onEdit(item,id) {
+    this.currentUserId = id;
     this.Botuserform.patchValue(item);
-    this.h4="Update Bot User"
-    this.buttonName="Update";
+    this.h4="Update Bot User";
+    window.scroll({
+      top: 0,
+      behavior: 'smooth'
+    });
   }
 
   public updateUser(item): void {
-    this.userservice.updateuser(item).then(res => console.log('res :>> ', res)).catch(err => console.log('err :>> ', err))
+    console.log(`===>item`, item.id)
+    this.userservice.updateuser(item,this.currentUserId).then(res => console.log('res :>> ', res)).catch(err => console.log('err :>> ', err));
+    this.toastr.success('User Data Update!');
+    this.Botuserform.reset();
+    this.h4 = 'Add Bot User';
   }
 
 }
