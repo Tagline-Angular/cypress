04565950dfd0ce27f214089c1069f7ab9159c39a
divangi-tagline <divangi.tagline@gmail.com>
2022-04-15 17:27:33 +0530

    code improvement - WIP

--- src/app/views/realuser/realuser.component.ts
+++ src/app/views/realuser/realuser.component.ts
@@ -1,27 +1,27 @@
-import { Component, OnInit } from '@angular/core';
-import { UserService } from '../../shared/service/user.service';
+import { Component, OnInit } from "@angular/core";
+import { UserService } from "../../shared/service/user.service";
 
 @Component({
-  selector: 'app-realuser',
-  templateUrl: './realuser.component.html',
-  styleUrls: ['./realuser.component.css']
+  selector: "app-realuser",
+  templateUrl: "./realuser.component.html",
+  styleUrls: ["./realuser.component.css"],
 })
 export class RealuserComponent implements OnInit {
-  public selectedtitle: string = '';
+  public selectedtitle: string = "";
   public users: any = [];
   public filterUserList: any = [];
 
   public search: boolean = false;
-  constructor(private userservice: UserService) { }
+  constructor(private userservice: UserService) {}
 
   ngOnInit(): void {
-    this.getAllUserList()
+    this.getAllUserList();
   }
 
   public searchByUserName(): void {
-    this.filterUserList = this.users.filter(item => item.name=== this.selectedtitle);
-    console.log('this.filterUserList :>> ', this.filterUserList);
-
+    this.filterUserList = this.users.filter(
+      (item) => item.name === this.selectedtitle
+    );
   }
 
   private getAllUserList(): void {
@@ -29,8 +29,6 @@ export class RealuserComponent implements OnInit {
       this.users = data.map((e) => {
         return Object.assign({ id: e.payload.doc.id }, e.payload.doc.data());
       });
-      console.log('this.users :>> ', this.users);
     });
-
   }
 }
