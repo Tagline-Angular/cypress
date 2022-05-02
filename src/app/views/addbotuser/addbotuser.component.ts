0c57f910f41784f2bc77aff290b3b8756520c9fb
divangi-tagline <divangi.tagline@gmail.com>
2022-05-02 15:13:39 +0530

    some changes in design

--- src/app/views/addbotuser/addbotuser.component.ts
+++ src/app/views/addbotuser/addbotuser.component.ts
@@ -4,9 +4,9 @@ import { UserService } from "../../shared/service/user.service";
 import { ToastrService } from "ngx-toastr";
 import * as moment from "moment";
 import { UsernameValidator } from "./username.validator";
-import { map, switchMap } from 'rxjs/operators';
+import { map, switchMap } from "rxjs/operators";
 import { combineLatest } from "rxjs";
-import * as _ from 'lodash';
+import * as _ from "lodash";
 import { AngularFirestore } from "@angular/fire/firestore";
 @Component({
   selector: "app-addbotuser",
@@ -22,7 +22,7 @@ export class AddbotuserComponent implements OnInit {
   public buttonName = "Add";
   public botPostsList = [];
   public deleteData: any = [];
-  public h4 = "Add Bot User";
+  public h4 = "Add Bot-User";
   public filterUserList: any = [];
   public commentedPosts = [];
   public postToDelete = [];
@@ -30,11 +30,14 @@ export class AddbotuserComponent implements OnInit {
     private userservice: UserService,
     private toastr: ToastrService,
     public fireStrore: AngularFirestore
-  ) { }
+  ) {}
 
   ngOnInit(): void {
     this.Botuserform = new FormGroup({
-      user_name: new FormControl(null, [Validators.required, UsernameValidator.noWhiteSpace]),
+      user_name: new FormControl(null, [
+        Validators.required,
+        UsernameValidator.noWhiteSpace,
+      ]),
       email: new FormControl(null, [
         Validators.required,
         Validators.email,
@@ -70,10 +73,10 @@ export class AddbotuserComponent implements OnInit {
             user_name: this.Botuserform.value.user_name,
             email: this.Botuserform.value.email,
             date: moment().format("YYYY-MM-DD HH:MM:SS.SSSSSS"),
-            phone: '',
-            platform: '',
+            phone: "",
+            platform: "",
             status_time: moment().format("YYYY-MM-DD HH:MM:SS.SSSSSS"),
-            token: 'token',
+            token: "token",
             token_created_at: moment().format("YYYY-MM-DD HH:MM:SS.SSSSSS"),
             token_updated_at: moment().format("YYYY-MM-DD HH:MM:SS.SSSSSS"),
           };
@@ -97,7 +100,6 @@ export class AddbotuserComponent implements OnInit {
     });
   }
 
-
   public handleDelete(id: string) {
     this.currentUserId = id;
     //Delete user post on deleted user start
@@ -105,7 +107,9 @@ export class AddbotuserComponent implements OnInit {
       this.botPostsList = res.map((e) => {
         return Object.assign({ id: e.payload.doc.id }, e.payload.doc.data());
       });
-      this.deleteData = this.botPostsList.filter((data) => data.uid === this.currentUserId);
+      this.deleteData = this.botPostsList.filter(
+        (data) => data.uid === this.currentUserId
+      );
     });
     //Delete user post on deleted user end
   }
@@ -115,7 +119,7 @@ export class AddbotuserComponent implements OnInit {
       let id = ele.id;
       this.userservice.removePost(id);
     });
-    
+
     this.userservice.remove(this.currentUserId);
     this.deleteBotLikesComments(this.currentUserId);
     this.toastr.success("User deleted!");
@@ -125,23 +129,31 @@ export class AddbotuserComponent implements OnInit {
 
   public deleteBotLikesComments(userId: string) {
     let allPosts: any;
-    this.userservice.getAllUserPosts()
+    this.userservice
+      .getAllUserPosts()
       .pipe(
         switchMap((res) => {
-          allPosts = res
-            .map((r: any) => {
-              let payload = Object.assign({ id: r.payload.doc.id }, r.payload.doc.data());
-              return this.userservice.getCommentsForPost(payload.id)
-                .pipe(
-                  map((comments: any) => {
-                    const data = {
-                      ...payload,
-                      comments: comments.map(e => Object.assign({ commentId: e.payload.doc.id }, { statusId: r.payload.doc.id }, e.payload.doc.data()))
-                    }
-                    return data
-                  })
-                )
-            });
+          allPosts = res.map((r: any) => {
+            let payload = Object.assign(
+              { id: r.payload.doc.id },
+              r.payload.doc.data()
+            );
+            return this.userservice.getCommentsForPost(payload.id).pipe(
+              map((comments: any) => {
+                const data = {
+                  ...payload,
+                  comments: comments.map((e) =>
+                    Object.assign(
+                      { commentId: e.payload.doc.id },
+                      { statusId: r.payload.doc.id },
+                      e.payload.doc.data()
+                    )
+                  ),
+                };
+                return data;
+              })
+            );
+          });
           return combineLatest(allPosts);
         })
       )
@@ -150,31 +162,30 @@ export class AddbotuserComponent implements OnInit {
         this.deleteLikesOfBot(allPosts, userId);
         const newArray = [];
         allPosts.forEach((post: any) => {
-          const _post = post.comments.filter((e: any) =>
-            e.commented_user_id == userId
-          )
+          const _post = post.comments.filter(
+            (e: any) => e.commented_user_id == userId
+          );
           if (_post.length) {
-            newArray.push(Object.assign(post, { filteredComments: _post }))
+            newArray.push(Object.assign(post, { filteredComments: _post }));
           }
-        })
+        });
         await newArray.forEach(async (newPost) => {
-          newPost.commentCount = newPost.commentCount - newPost.filteredComments.length;
+          newPost.commentCount =
+            newPost.commentCount - newPost.filteredComments.length  ;
           await this.userservice.deleteBotComments(newPost);
           setTimeout(async () => {
             await delete newPost.filteredComments;
             await delete newPost.comments;
             await this.userservice.updateStatus(newPost, newPost.id);
           }, 0);
-        })
+        });
       });
   }
 
   public deleteLikesOfBot(allPosts: any, userId: any) {
     let likedPost = [];
     likedPost = allPosts.filter((post: any) =>
-      post.liked_user_ids?.find(likedIds =>
-        likedIds === userId
-      )
+      post.liked_user_ids?.find((likedIds) => likedIds === userId)
     );
 
     likedPost.forEach((post: any) => {
@@ -182,7 +193,7 @@ export class AddbotuserComponent implements OnInit {
       let deleteIndex = post.liked_user_ids.indexOf(userId);
       post.liked_user_ids.splice(deleteIndex, 1);
       this.userservice.updateStatus(post, post.id);
-    })
+    });
   }
 
   public async deleteCommentsOfBot(allPosts: any, userId: any) {
@@ -191,44 +202,50 @@ export class AddbotuserComponent implements OnInit {
     this.commentedPosts = [];
     this.postToDelete = [];
     allPosts.forEach((post: any) => {
-      this.userservice.getCommentsForPost(post.id).subscribe(async (res: any) => {
-        this.commentedPosts.push(res.map((e) => {
-          return Object.assign({ statusId: post.id }, { commentId: e.payload.doc.id }, e.payload.doc.data());
-        })) // get all posts having comments from any user
+      this.userservice
+        .getCommentsForPost(post.id)
+        .subscribe(async (res: any) => {
+          this.commentedPosts.push(
+            res.map((e) => {
+              return Object.assign(
+                { statusId: post.id },
+                { commentId: e.payload.doc.id },
+                e.payload.doc.data()
+              );
+            })
+          ); // get all posts having comments from any user
 
-        // commentedPosts.forEach((post) => {
-        //   if (post.commented_user_id == userId) {
-        //     postToDelete.push(post)
-        //   }
-        // }
+          // commentedPosts.forEach((post) => {
+          //   if (post.commented_user_id == userId) {
+          //     postToDelete.push(post)
+          //   }
+          // }
 
-        this.postToDelete.push(
-          this.commentedPosts.filter((commentPost) =>
-            commentPost.commented_user_id == userId
-          )
-        )
+          this.postToDelete.push(
+            this.commentedPosts.filter(
+              (commentPost) => commentPost.commented_user_id == userId
+            )
+          );
 
-
-        // if (this.postToDelete && this.postToDelete.length) {
-        //   this.postToDelete.forEach((deletepost) => {
-        //     this.userservice.deleteBotComments(deletepost, post); // to delete comments
-        //     // post.commentCount -= 1;
-        //     // this.userservice.decreaseBotCommentCount(post); // to decrease count
-        //   })
-        // }
-      })
-
-    })
-
-    this.postToDelete = await this.commentedPosts.filter((commentPost) =>
-      commentPost.commented_user_id == userId
-    )
+          // if (this.postToDelete && this.postToDelete.length) {
+          //   this.postToDelete.forEach((deletepost) => {
+          //     this.userservice.deleteBotComments(deletepost, post); // to delete comments
+          //     // post.commentCount -= 1;
+          //     // this.userservice.decreaseBotCommentCount(post); // to decrease count
+          //   })
+          // }
+        });
+    });
+    console.log('this.postToDelete :>> ', this.postToDelete);
+    this.postToDelete = await this.commentedPosts.filter(
+       (commentPost) => commentPost.commented_user_id == userId
+    );
   }
   onEdit(item, id) {
     this.currentUserId = id;
-    this.Botuserform.patchValue(item);
+    this.Botuserform.patchValue(item);  
     this.h4 = "Update Bot User";
-    this.buttonName = "update"
+    this.buttonName = "update";
     window.scroll({
       top: 0,
       behavior: "smooth",
@@ -251,5 +268,5 @@ export class AddbotuserComponent implements OnInit {
 
   get formControl() {
     return this.Botuserform.controls;
-  } 
+  }
 }
