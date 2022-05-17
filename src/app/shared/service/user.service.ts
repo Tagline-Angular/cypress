fff251d5a6684c684b32f542f716cf65e2bb92b6
divangi-tagline <divangi.tagline@gmail.com>
2022-05-17 17:00:47 +0530

    replaced dummy data to real user

--- src/app/shared/service/user.service.ts
+++ src/app/shared/service/user.service.ts
@@ -49,7 +49,7 @@ export class UserService {
   }
 
   public getAllUserList() {
-    return this.firestore.collection("DummyUser").snapshotChanges();
+    return this.firestore.collection("Users").snapshotChanges();
   }
 
   //like and comments update in firebase
